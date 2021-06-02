d54efe84ebdf7c2dd566332eb84d2613aece8dd4
max99xam <pepetrov123@list.ru>
2021-06-02 14:25:55 +0300

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 import express from "express";
+import config from "config";
+import mongoose from "mongoose";
 
 const app = express();
+const PORT = config.get("port") || 5000;
+
+async function start(){
+    try{
+       // connect to database
+       await mongoose.connect(config.get("mongoUrl"), {
+           useInifiedTopology: true,
+           useNewUrlParser: true
+       });
+
+       // init app server
+       app.listen(PORT, () => {
+         console.log(`Port has been started on port: ${PORT}...`);
+       });
+
+    }catch(e){
+        console.log("Server error: ", e.message);
+        process.exit(1);
+    }
+}
+
+start();
+
