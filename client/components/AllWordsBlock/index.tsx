ad814a92d18bda4313be094fdc86bd7befd6f445
max99xam <pepetrov123@list.ru>
2021-08-02 23:15:15 +0300

    redux works corectly

--- client/components/AllWordsBlock/index.tsx
+++ client/components/AllWordsBlock/index.tsx
@@ -4,7 +4,7 @@ import { WordsTable } from 'components';
 import { useTypedSelector } from 'hooks/useTypedSelector';
 
 function AllWordsBlock() {
-  const allWords = useTypedSelector((state) => state.allWords);
+  const { words } = useTypedSelector((state) => state.allWords);
 
   React.useEffect(() => {
     try {
@@ -18,11 +18,11 @@ function AllWordsBlock() {
       <Typography variant="h5" component="h2" gutterBottom>
         All words.
       </Typography>
-      <WordsTable words={allWords} />
+      <WordsTable words={words} />
       <Typography variant="h5" component="h2" gutterBottom>
         My dict.
       </Typography>
-      <WordsTable words={allWords} />
+      <WordsTable words={words} />
     </>
   );
 }
