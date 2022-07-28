5f6b1dea42b544867918a2c436575fdf6b3b8a94
max <kramax42@yandex.by>
2022-07-28 13:56:49 +0300

    moved useUserWordsCount hook to common exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useCommonWords } from './useCommonWords';
 export { useUserWords } from './useUserWords';
+export { useUserWordsCount } from './useUserWords';
 export { useUser } from './useUser';
