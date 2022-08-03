44d9debd9e88878f9f012b976a66c5ce8471f13b
max <kramax42@yandex.by>
2022-08-03 12:52:30 +0300

    updated navbar styles

--- src/layouts/NavBar/NavBar.tsx
+++ src/layouts/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Menu, Button, Label } from 'semantic-ui-react';
+import { Menu, Button, Label, Container } from 'semantic-ui-react';
 import { useRouter } from 'next/router';
 import styles from './NavBar.module.scss';
 import { logout } from 'libs/auth.api';
@@ -16,44 +16,46 @@ export const NavBar = () => {
 
 	return (
 		<>
-			<Menu stackable className={styles.menu}>
-				<Menu.Item>
-					<img alt="logo" src="https://react.semantic-ui.com/logo.png" />
-				</Menu.Item>
-
-				<Menu.Menu position="right">
-
-					<Menu.Item name="Add word" link active={router.pathname === '/add-word'} onClick={() => router.push('/add-word')}>
-						{/* <Link href="/add-word">Add word</Link> */}
-						{/* <Button icon='add' size='small'  /> */}
+			<Menu secondary stackable className={styles.menu}>
+				<Container>
+					<Menu.Item>
+						<h2>English words</h2>
 					</Menu.Item>
 
-					<Menu.Item name="All words" link active={router.pathname === '/common-words'} onClick={() => router.push('/common-words')}>
-						{/* <Link href="/">All words</Link> */}
-					</Menu.Item>
+					<Menu.Menu position="right">
 
-					<Menu.Item name="My words" link active={router.pathname === '/'} onClick={() => router.push('/')}>
-						{/* <Link href="/">All words</Link> */}
-					</Menu.Item>
+						<Menu.Item name="Add word" link active={router.pathname === '/add-word'} onClick={() => router.push('/add-word')}>
+							{/* <Link href="/add-word">Add word</Link> */}
+							{/* <Button icon='add' size='small'  /> */}
+						</Menu.Item>
 
-					{loggedIn ? (
-						<Menu.Item name="user"><Label style={{ margin: 0 }} size="big" icon="user" color='green' content={user?.name} /></Menu.Item>
-					) : (
-						<Menu.Item name="sign-in">Sign-in</Menu.Item>
-					)}
-					{loggedIn ? (
-						loading ? (
-							'loading'
+						<Menu.Item name="All words" link active={router.pathname === '/common-words'} onClick={() => router.push('/common-words')}>
+							{/* <Link href="/">All words</Link> */}
+						</Menu.Item>
+
+						<Menu.Item name="My words" link active={router.pathname === '/'} onClick={() => router.push('/')}>
+							{/* <Link href="/">All words</Link> */}
+						</Menu.Item>
+
+						{loggedIn ? (
+							<Menu.Item name="user"><Label style={{ margin: 0 }} size="big" icon="user" color='green' content={user?.name} /></Menu.Item>
 						) : (
-							<Menu.Item >
-								{/* <Icon name='log out' size='big' className='ui transparent icon input'/> */}
-								<Button basic icon="log out" size="big" content='Logout' onClick={logoutHandler} />
-							</Menu.Item>
-						)
-					) : (
-						<Menu.Item name="sign-up">Sign-up</Menu.Item>
-					)}
-				</Menu.Menu>
+							<Menu.Item name="sign-in">Sign-in</Menu.Item>
+						)}
+						{loggedIn ? (
+							loading ? (
+								'loading'
+							) : (
+								<Menu.Item >
+									{/* <Icon name='log out' size='big' className='ui transparent icon input'/> */}
+									<Button basic icon="log out" size="big" content='Logout' onClick={logoutHandler} />
+								</Menu.Item>
+							)
+						) : (
+							<Menu.Item name="sign-up">Sign-up</Menu.Item>
+						)}
+					</Menu.Menu>
+				</Container>
 			</Menu>
 		</>
 	);
